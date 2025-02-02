6726952851724a05d09bb6e31d89df72be0fd838
itsmejv404 <+>
2025-02-02 21:19:30 +0530

    contestForm

--- components/contestForm.jsx
+++ components/contestForm.jsx
@@ -8,6 +8,10 @@ export default function ContactUs() {
   const [gameplayLink,setGameplayLink] = useState('');
   
   function sendMessage() {
+    if (phoneNumber.length != 10){
+      alert("Invalid Phone Number")
+      return false;
+    }
     const webhookUrl = "https://discord.com/api/webhooks/1334929752154308690/a2RMhoXf_FreQ6IPkEwZtTvO7J8G3bfcMeSF6QVSxFZFpqq4EuHffKwq4V_eEIIoeMUo";
 
     const payload1 = {
@@ -21,8 +25,10 @@ export default function ContactUs() {
         request.open("POST", webhookUrl);
         request.setRequestHeader("Content-type", "application/json");
         request.send(JSON.stringify(payload));
+        return true;
     }
-    sendPayload(payload1);
+    return(sendPayload(payload1));
+
   }
   const handleNameChange = (event) => {
     const value = event.target.value;
@@ -37,16 +43,15 @@ export default function ContactUs() {
     // Allow only numbers, limit to 10 digits
     if (/^\d{0,10}$/.test(value)) {
       setPhoneNumber(value);
+      if(value.length > 10){
+        alert(value.length)
+      }
     }
   };
   
   const handleEmailAddressChange = (event) => {
     const value = event.target.value;
-    // Basic email format validation
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if (emailRegex.test(value)) {
-      setEmailId(value);
-    }
+    setEmailId(value);
   };
   
   const handleGameplayLink = (event) => {
@@ -62,9 +67,13 @@ export default function ContactUs() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setEmailId('')
-    
-    sendMessage();
+    if(sendMessage()){
+      setName('')
+      setPhoneNumber('')
+      setEmailId('')
+      setGameplayLink('')
+      alert("Contest Registration Successful !!")
+    }
   };
   return (
     <div id='ContactUs' className={styles.contactContainer}>
@@ -76,7 +85,7 @@ export default function ContactUs() {
         
         <div className={styles.contactContainerForm}>
           <form onSubmit={handleSubmit}>
-          <div className={styles.field}><h4>Name : </h4><input placeholder='Enter your Email' type='text' onChange={handleNameChange} required value={name}/></div>
+          <div className={styles.field}><h4>Name : </h4><input placeholder='Enter your Name' type='text' onChange={handleNameChange} required value={name}/></div>
           <div className={styles.field}><h4>Phone Number</h4><input placeholder='Enter your Phone Number' type='text' onChange={handlePhoneNumberChange} required value={phoneNumber}/></div>
           <div className={styles.field}><h4>Email Address</h4><input placeholder='Enter your Email Address' type='email' onChange={handleEmailAddressChange} required value={emailId}/></div>
           <div className={styles.field}><h4>Gameplay Link</h4><input placeholder='Enter your Gameplay Link' type='text' onChange={handleGameplayLink} required value={gameplayLink}/></div>
